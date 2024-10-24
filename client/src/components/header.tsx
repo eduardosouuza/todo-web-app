673f6cdf9ca910c5a460c7e668287c3577a08a56
Marcos <marcosnorato@outlook.com>
2024-10-23 22:24:38 -0300

    Cria o frontend

---
+++ client/src/components/header.tsx
@@ -0,0 +1,9 @@
+export function Header() {
+  return (
+    <header className="flex h-48 items-center justify-center gap-4 bg-slate-100">
+      <h1 className="text-4xl font-black text-slate-500">
+        to<span className="text-slate-950">do</span>
+      </h1>
+    </header>
+  )
+}
