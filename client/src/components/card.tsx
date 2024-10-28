b25fec6869309d53532c582d782059ba4ac93769
Wallauerr <wallauer@protonmail.com>
2024-10-27 23:14:19 -0300

    [frontend] minimal adjusts

--- client/src/components/card.tsx
+++ client/src/components/card.tsx
@@ -13,8 +13,8 @@ export function Card({ task, onDelete, checked, ...props }: CardProps) {
   return (
     <div
       className={clsx(
-        'flex items-center justify-between gap-3 rounded-lg bg-zinc-200 p-4 text-sm text-slate-950 hover:bg-zinc-200/70',
-        checked && 'bg-transparent ring ring-slate-500',
+        'flex items-center justify-between gap-3 rounded-lg bg-zinc-200 p-4 text-sm text-zinc-950 hover:bg-zinc-200/70',
+        checked && 'bg-transparent ring ring-zinc-500',
       )}
     >
       <div className="flex items-center gap-3">
@@ -24,7 +24,7 @@ export function Card({ task, onDelete, checked, ...props }: CardProps) {
           className="size-5 cursor-pointer rounded-full border-none text-lime-500 outline-none focus:ring-0"
           {...props}
         />
-        <p className={clsx(checked && 'text-slate-500 line-through')}>{task}</p>
+        <p className={clsx(checked && 'text-zinc-500 line-through')}>{task}</p>
       </div>
       <button onClick={onDelete}>
         <Trash2 className="size-5 text-zinc-500 duration-200 hover:text-red-500" />
