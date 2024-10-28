6224c9fd685b44b02dd937476b6384acf9001b4d
Wallauerr <wallauer@protonmail.com>
2024-10-27 22:31:12 -0300

    [frontend] change header title and add main color

--- client/src/components/header.tsx
+++ client/src/components/header.tsx
@@ -1,8 +1,8 @@
 export function Header() {
   return (
     <header className="flex h-48 items-center justify-center gap-4">
-      <h1 className="text-4xl font-black text-slate-500">
-        to<span className="text-slate-950">do</span>
+      <h1 className="text-4xl font-black text-zinc-500">
+        Task <span className="text-zinc-950">Master</span>
       </h1>
     </header>
   )
