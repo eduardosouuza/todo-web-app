cdb11c039afcdb18a9f150a94d7fd55ff8c13bd3
Wallauerr <wallauer@protonmail.com>
2024-11-04 20:50:56 -0300

    [frontend] create button toggle and add the button in header

--- client/src/components/header.tsx
+++ client/src/components/header.tsx
@@ -1,9 +1,13 @@
+import { ModeToggle } from "./mode-toggle";
+
 export function Header() {
   return (
     <header className="flex h-48 items-center justify-center gap-4">
       <h1 className="text-4xl font-black text-zinc-500">
         Task <span className="text-zinc-950">Master</span>
       </h1>
+
+      <ModeToggle />
     </header>
   )
 }
