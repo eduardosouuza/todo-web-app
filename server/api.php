a8705d72298b2808bc73e7880e1677c525f0aae8
Wallauerr <wallauer@protonmail.com>
2024-11-28 23:45:11 -0300

    [backend] initial get structure

---
+++ server/api.php
@@ -0,0 +1,14 @@
+<?php
+require __DIR__ . '/database/conn.php';
+
+header('Content-Type: application/json');
+header('Access-Control-Allow-Origin: *');
+header('Access-Control-Allow-Methods: GET, POST, PUT, DELETE');
+
+if ($_SERVER['REQUEST_METHOD'] === 'GET') {
+    $stmt = $pdo->prepare("SELECT * FROM task");
+    $stmt->execute();
+    $tasks = $stmt->fetchAll(PDO::FETCH_ASSOC);
+    echo json_encode($tasks);
+}
+
