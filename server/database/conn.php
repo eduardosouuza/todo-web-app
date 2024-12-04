52cc9bdbd9a56470f814f0b148edb6dda436ca22
Wallauerr <wallauer@protonmail.com>
2024-12-03 21:55:18 -0300

    [backend] adjust db conection params

--- server/database/conn.php
+++ server/database/conn.php
@@ -1,12 +1,12 @@
 <?php
 
 $hostname = 'localhost';
-$database = 'to_do';
+$database = 'to_do_list';
 $username = 'postgres';
-$password = '3525';
+$password = '1234';
 
 try {
     $pdo = new PDO("pgsql:host=$hostname;dbname=$database", $username, $password);
 } catch (PDOException $e) {
-    echo 'Erro: '. $e->getMessage();
+    echo "Erro: " . $e->getMessage();
 }
